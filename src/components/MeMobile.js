705c07b39d5486445d6aa92301c6246f51c36742
harsha.venkatram <harsha.venkatram@sakhatech.com>
2019-04-04 15:22:03 +0530

    Adding mobile responsive

--- src/components/MeMobile.js
+++ src/components/MeMobile.js
@@ -22,36 +22,48 @@ const MeMobile = () => (
           <div className="row m-0">
             <div className="col-4 d-flex flex-column align-items-center pl-0">
               <Link to="/designs" className="d-flex flex-column align-items-center">
-                <div className="mobicon mobicon-designs"></div>
+                <div className="mobicon mobicon-designs">
+                  <i className="icon-design" />
+                </div>
                 <span className="mobicon-title">Designs</span>
               </Link>
             </div>
             <div className="col-4 d-flex flex-column align-items-center">
               <Link to="/code" className="d-flex flex-column align-items-center">
-                <div className="mobicon mobicon-code"></div>
+                <div className="mobicon mobicon-code">
+                  <i className="icon-code" />
+                </div>
                 <span className="mobicon-title">Code</span>
               </Link>
             </div>
             <div className="col-4 d-flex flex-column align-items-center pr-0">
-              <div className="mobicon mobicon-experience"></div>
+              <div className="mobicon mobicon-experience">
+                <i className="icon-experience" />
+              </div>
               <span className="mobicon-title">Experience</span>
             </div>
           </div>
 
           <div className="row mx-0 mt-4">
             <div className="col-4 d-flex flex-column align-items-center pl-0">
-              <div className="mobicon mobicon-links"></div>
+              <div className="mobicon mobicon-links">
+                <i className="icon-links" />
+              </div>
               <span className="mobicon-title">Links</span>
             </div>
             <div className="col-4 d-flex flex-column align-items-center">
               <Link to="/artwork" className="d-flex flex-column align-items-center">
-                <div className="mobicon mobicon-artwork"></div>
+                <div className="mobicon mobicon-artwork">
+                  <i className="icon-art" />
+                </div>
                 <span className="mobicon-title">Artwork</span>
               </Link>
             </div>
             <div className="col-4 d-flex flex-column align-items-center pr-0">
               <Link to="/photography" className="d-flex flex-column align-items-center">
-                <div className="mobicon mobicon-photography"></div>
+                <div className="mobicon mobicon-photography">
+                  <i className="icon-camera" />
+                </div>
                 <span className="mobicon-title">Photography</span>
               </Link>
             </div>
